8c98e4db095e071288ad98f9df2008b0930d308b
tamiration <tamircohen1@gmail.com>
2015-07-10 20:29:51 +0300

    changes
    Hi , There all
    i removed some of the old code. i made a few new classes like
    Submarine,Game and Player.
    i need to think how display_board  function will act.
    farewell for now my friends

--- Submarines.py
+++ Submarines.py
@@ -1,31 +1,19 @@
 
-        
-class Board :
 
-    # This function gets size of  a requested board game as a tuple
-    # and returns the board as list of lists
-    def __init__(self):
+class Board  (object):
+
+    #
+    def __init__(self,size):
         self.board=[]
-        self.dead = '1'
-        self.alive = '2'
-        self.empty = '0'
-        self.attack = '3'
-        self.place = '4'
-
-    def create_Board(self,size):
         self.size=size
         for i in xrange(size[0]):
-            display=''
             self.board.append([])
             for j in xrange(size[1]):
-                self.board[i].append(self.empty)
-                #print self[i][j]
-                display += self.board[i][j]
-            print display
-            print "\n"
-        print self.board[3][4]
+                self.board[i].append("???")
         return self.board
-    def disply_board(self):
+
+    # not relevant i need to recevie the coordinate from the submarines
+    def display_board(self):
         display=''
         for i in xrange(self.size[0]):
             for j in xrange(self.size[1]):
@@ -33,77 +21,67 @@ class Board :
             display += "\n"
         print display
 
+class Submarine (object):
+    def __init__(self):
 
+        self.lifeParts=[]
+        self.coordinates = []
+        self.size = int(raw_input(" put the size of your submarine: "))
+        # request input from  the player for it's coordinates and size
+        print ("put the coridinates of your submarine one piece at a time. \n" /
+                              " for example : 3,4")
+        for i in self.size :
+            self.coordinates.append(raw_input("put your cordinate: "))
+            self.lifeParts.append(False)
+        print "You have finished to place your submarine"
 
-    # This function gets the Points of the start and the end of the submarine
-    # and takes an action to attack or place it.
-    def take_action(self,start_point,end_point,action):
-         if self.check_position(start_point,end_point) == self.empty:
-             if start_point.getY() - end_point.getY() != 0 :
-                 for i in xrange(start_point.getY(),end_point.getY()+1):
-                     self.board[start_point.getX()][i]=action
-             elif start_point.getX() - end_point.getX() != 0 :
-                 for i in xrange(start_point.getX(),end_point.getX()+1):
-                     self.board[start_point.getY()][i]=action
-
-    def attack_unit (self, ):
-
-        Point start
-        startX= raw_input('put the start point of your unit: ')
-
-        self.Point end = raw_input('put the end point of your unit: ')
-        self.take_action(start,end,self.attack)
-    def place_unit(self):
-        self.take_action(start,end,self.place)
-
-    # This function gets start , end point and size of the submrine.
-    # the function returns if it alive,dead or empty.
-    def check_position(self,start_point,end_point):
-         if start_point.getX()==end_point.getX():
-             for i in xrange(start_point.getY(),end_point.getY()+1):
-                 if self.board[start_point.getX()][i] == self.alive:
-                     return self.alive
-                 elif self.board[start_point.getX()][i] ==self.dead:
-                     return self.dead
-             return self.empty
-         elif start_point.getY()==end_point.getY():
-                for i in xrange(start_point.getX(),end_point.getX()+1):
-                 if self.board[start_point.getX()][i] == self.alive :
-                     return self.alive
-                 elif self.board[start_point.getX()][i] ==self.dead:
-                    return self.dead
-         else:
-              raise ValueError('Enter two points in same x or y cordinates')
-         return self.empty
+    def status (self,attackCoordinate):
+        for i in self.coordinates:
+            if self.coordinates[i]== attackCoordinate:
+                self.lifeParts[i] = True
+                if False not in self.lifeParts:
+                    return "The Submarine destroyed"
+                else:
+                    return "This was a Hit"
+        return "This was a miss!!"
 
 
 
 
-class Point ():
-    def __init__(self,x,y):
-        self.x = x
-        self.y = y
+class Player (object):
 
-    def getX (self):
-        return self.x
+    def __init__(self,numUnits):
+        self.score=0
+        self.numUnits = numUnits
+        self.submarines = []
+        self.name = raw_input("Enter your name: ")
 
-    def getY (self):
-        return self.y
 
-    def setX (self,x):
-        self.x = x
+    def place_units(self):
+        for i in self.numUnits:
+            self.submarines.append(Submarine())
 
-    def setY (self,y):
-        self.y =y
+
+    def attack(self):
+        coordinate = raw_input("put the cordinate of your attack: ")
+
+class Game (object):
+    def __init__(self):
+        self.numUnits = raw_input("Enter number of maximun units to each player: ")
+        self.board_size = raw_input("Enter the size of the board like this \n 5,5 or 6,3")
+        self.player1 = Player(self.numUnits)
+        self.player2 = Player(self.numUnits)
+        self.board = Board(self.board_size)
+
+
+
 
         
 
-class main:
-    b1 = Board()
-    b1.create_Board((5, 5))
-    b1.unit_position(Point ( 1, 1), Point  ( 1, 4) )
-    b1.disply_board()
-    print (type (1))
+def main(self):
+    game = Game()
+
+
 
 if __name__ == '__main__':
         main()
